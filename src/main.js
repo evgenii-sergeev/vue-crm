9daa61d0c6e869124a509196b6d849570a903eb1
Evgenii Sergeev <evgenii.s@devstark.com>
2020-11-17 20:51:05 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'materialize-css/dist/js/materialize.min'
+import dateFilter from '@/filters/date.filter'
+
+Vue.filter('date', dateFilter)
 
 Vue.config.productionTip = false
 
