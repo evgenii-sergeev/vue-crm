174628ca74f0e02640953428704891674ec57602
Evgenii Sergeev <evgenii.s@devstark.com>
2020-11-19 11:22:49 +0300

    firebase connected auth added

--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ import store from './store'
 import 'materialize-css/dist/js/materialize.min'
 import dateFilter from '@/filters/date.filter'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
 Vue.use(Vuelidate)
 Vue.use(messagePlugin)
 Vue.filter('date', dateFilter)
@@ -14,8 +18,27 @@ Vue.filter('date', dateFilter)
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+var firebaseConfig = {
+  apiKey: "AIzaSyDm9JBRPqnTxdv6LYHPGrm_ixP-DFlyRhg",
+  authDomain: "vue-crm-fbase.firebaseapp.com",
+  databaseURL: "https://vue-crm-fbase.firebaseio.com",
+  projectId: "vue-crm-fbase",
+  storageBucket: "vue-crm-fbase.appspot.com",
+  messagingSenderId: "217428948439",
+  appId: "1:217428948439:web:5e488ef4d0fa73ea350792",
+  measurementId: "G-ZC8T4NLZ69"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+let app
+
+firebase.auth().onAuthStateChanged( () => {
+  if(!app) {
+    app = new Vue({
+            router,
+            store,
+            render: h => h(App)
+          }).$mount('#app')
+  }
+})
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -69,7 +69,7 @@ export default {
     password: {required, minLength: minLength(6)}
   },
   methods: {
-    onSubmit() {
+    async onSubmit() {
       if(this.$v.$invalid) {
         this.$v.$touch();
         return;
@@ -78,8 +78,12 @@ export default {
         email: this.email,
         password: this.password
       }
-      console.log(formData)
-      this.$router.push('/');
+      try {
+        await this.$store.dispatch('login', formData)
+        this.$router.push('/');
+      } catch (e) {
+        console.log(e)
+      }
     }
   },
   mounted() {
